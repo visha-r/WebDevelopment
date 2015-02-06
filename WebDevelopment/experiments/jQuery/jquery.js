c3bf24da23420eb79b5fe5d9299dae5dc65a4802
visha-r <rajaramchettiar.v@husky.neu.edu>
2015-02-05 21:25:44 -0500

    jQuery basic elements

---
+++ WebDevelopment/experiments/jQuery/jquery.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+    $("#red").click(function () {
+        $("#myBox").css("background-color", "red");
+    });
+
+    $("#blue").click(function () {
+        $("#myBox").css("background-color", "blue");
+    });
+
+    $("#clear").click(function () {
+        $("#myBox").css("background-color", "white");
+    });
+
+    $("#hide").click(function () {
+        $("#myBox").hide();
+        $(".buttonGroup").hide();
+    });
+
+    $("#show").click(function () {
+        $("#myBox").show();
+        $(".buttonGroup").show();
+    });
+
+    $("#animate").click(function () {
+        $("#myBox").animate({ right: '50px' });
+    });
+
+    $("#animateCenter").click(function () {
+        $("#myBox").animate({ right: '520px' });
+    });
+
+    $("#fade").click(function () {
+        $("#myBox").fadeTo("slow", 0.15);
+    });
+
+    $("#brighten").click(function () {
+        $("#myBox").fadeTo("slow", 1);
+    });
+});
