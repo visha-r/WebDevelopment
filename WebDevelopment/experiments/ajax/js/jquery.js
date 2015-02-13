3e8c1953bede5295697a859b3c8f0a986ea79f78
visha-r <rajaramchettiar.v@husky.neu.edu>
2015-02-12 20:16:25 -0500

    Ajax movie search

---
+++ WebDevelopment/experiments/ajax/js/jquery.js
@@ -0,0 +1,49 @@
+﻿$(document).ready(function () {
+    $(window).load(function () {
+        $(".movieholder").hide();
+    });
+
+    $(".go").click(fetchData);
+
+    function fetchData(event) {
+        event.preventDefault();
+        var apikey = "325pdck85e9sd5fsfpy4d7j5";
+        var baseUrl = "http://api.rottentomatoes.com/api/public/v1.0";
+        var moviesSearchUrl = baseUrl + '/movies.json?format=jsonp&show=all&apikey=' + apikey;
+        var title = $("input[name=title]").val();
+        var year = $("input[name=year]").val();
+        var parameters = {
+            "title": $("input[name=title]").val()
+        }
+
+        $.ajax({
+            "url": moviesSearchUrl + "&q=" + title,
+            "dataType": "jsonp",
+            "success": function (response) {
+                $(".response").val(response);
+                renderUi(response.movies);
+            }
+        })
+    }
+
+    function renderUi(json) {
+        console.log(json);
+        var resultSet = $(".result-set");
+        var instanceTemplate = $(".result-set .instance:first").clone();
+        resultSet.empty();
+        for (var i = 0; i < json.length; i++) {
+            var instance = json[i];
+            var instanceDom = instanceTemplate.clone();
+            instanceDom.find(".title").html(instance.title);
+            instanceDom.find(".year").html(instance.year);
+            instanceDom.find(".audienceScore").html(instance.ratings.audience_score);
+            instanceDom.find(".criticsScore").html(instance.ratings.critics_score);
+            instanceDom.find(".rating").html(instance.mpaa_rating);
+            instanceDom.find(".year").html(instance.year);
+            instanceDom.find(".poster .image").attr("src", instance.posters.detailed);
+            resultSet.append(instanceDom);
+        }
+        $(".movieholder").show();
+    }
+
+});
---
+++ WebDevelopment/experiments/angular/js/include.js
@@ -0,0 +1,66 @@
+﻿/**
+ * javascript to include navigation menu in every page
+ **/
+
+document.write("<ul class='nav nav-tabs'>");
+document.write("<li role='presentation' class='music active' ng-click='music()'><a href='#'>Music</a></li>");
+document.write("<li role='presentation' class='software' ><a href='#' ng-click='software()'>Software</a></li>");
+document.write("<li role='presentation' class='movie' ><a href='#' ng-click='movie()'>Movie</a></li>");
+document.write("</li>");
+document.write("</ul>");
+$(document).ready(function () {
+    $(window).load(function () {
+        $(".prdtTable").hide();
+    });
+
+    angular.module('ionicApp', [])
+    .controller('MainCtrl', function ($scope, $http) {
+
+        // method to load music products
+        $scope.music = function () {
+            $http.jsonp('http://api.remix.bestbuy.com/v1/products(type=Music)?format=json&show=all&callback=JSON_CALLBACK&apiKey=wu48f36djjruzarbeahrx8ay')
+             .success(function (resp) {
+                 alert('inside music');
+                 $scope.products = resp.products;
+             })
+            .error(function (data) {
+                $scope.data = "Request failed";
+            });
+            $(".movie").removeClass("active");
+            $(".software").removeClass("active");
+            $(".music").addClass("active");
+            $(".prdtTable").show();
+        };
+
+        // Method to load movie products
+        $scope.movie = function () {
+            $http.jsonp('http://api.remix.bestbuy.com/v1/products(type=movie)?format=json&show=all&callback=JSON_CALLBACK&apiKey=wu48f36djjruzarbeahrx8ay')
+            .success(function (resp) {
+                $scope.products = resp.products;
+            })
+            .error(function (data) {
+                $scope.data = "Request failed";
+            });
+            $(".music").removeClass("active");
+            $(".software").removeClass("active");
+            $(".movie").addClass("active");
+            $(".prdtTable").show();
+        };
+
+        // Method to load software products
+        $scope.software = function () {
+            $http.jsonp('http://api.remix.bestbuy.com/v1/products(type=software)?format=json&show=all&callback=JSON_CALLBACK&apiKey=wu48f36djjruzarbeahrx8ay')
+            .success(function (resp) {
+                $scope.products = resp.products;
+            })
+            .error(function (data) {
+                $scope.data = "Request failed";
+            });
+            $(".music").removeClass("active");
+            $(".movie").removeClass("active");
+            $(".software").addClass("active");
+            $(".prdtTable").show();
+        };
+
+    });
+});
