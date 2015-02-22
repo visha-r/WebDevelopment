aba8d78bd0b81c321863c22c1ffcacf178ed3556
visha-r <rajaramchettiar.v@husky.neu.edu>
2015-02-21 23:02:45 -0500

    Basic angularJS routing experiment

--- WebDevelopment/experiments/angular/js/RoutingController.js
+++ WebDevelopment/experiments/angular/js/RoutingController.js
@@ -2,23 +2,53 @@
 app.controller('RoutingController', function ($scope, $route) {
 });
 
+$(document).ready(function () {
+    $("#loginlink").click(function () {
+        $("#loginlink").addClass("active");
+        $("#homelink").removeClass("active");
+        $("#favoritelink").removeClass("active");
+        $("#searchlink").removeClass("active");
+    });
+
+    $("#homelink").click(function () {
+        $("#homelink").addClass("active");
+        $("#loginlink").removeClass("active");
+        $("#searchlink").removeClass("active");
+        $("#favoritelink").removeClass("active");
+    });
+
+    $("#searchlink").click(function () {
+        $("#searchlink").addClass("active");
+        $("#loginlink").removeClass("active");
+        $("#homelink").removeClass("active");
+        $("#favoritelink").removeClass("active");
+    });
+
+    $("#favoritelink").click(function () {
+        $("#favoritelink").addClass("active");
+        $("#searchlink").removeClass("active");
+        $("#loginlink").removeClass("active");
+        $("#homelink").removeClass("active");
+    });
+});
+
 app.config(function ($routeProvider) {
     $routeProvider
         .when('/home', {
-            templateUrl: 'Wallpaper.html'
+            templateUrl: '../partials/Wallpaper.html'
         })
 
         .when('/login', {
-            templateUrl: 'Login.html'
+            templateUrl: '../partials/Login.html'
         })
 
         .when('/search', {
-         templateUrl: 'search.html',
+         templateUrl: '../partials/search.html',
          controller: 'searchController'
      })
 
     .when('/favorites', {
-        templateUrl: 'NoFavorites.html'
+        templateUrl: '../partials/NoFavorites.html'
     });
 
 });
